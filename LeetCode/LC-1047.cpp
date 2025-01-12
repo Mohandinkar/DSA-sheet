9a87ce596d71b0088747946d12575508f846719e
Mohandinkar <mohandinkar75@gmail.com>
2025-01-12 12:22:47 +0530

    LC-1047

---
+++ LeetCode/LC-1047.cpp
@@ -0,0 +1,25 @@
+// 1047 --> Remove All Adjacent Duplicates In String
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans = "";
+
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            char currChar = s[i];
+
+            if(ans == ""){
+                ans.push_back(currChar);
+            }
+            else if(currChar == ans.back()){
+                ans.pop_back();
+            }
+            else if(currChar != ans.back()){
+                ans.push_back(currChar);
+            }
+        }
+
+        return ans;
+    }
+};
