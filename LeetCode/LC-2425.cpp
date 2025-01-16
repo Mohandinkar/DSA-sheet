1548519ed0618a2735632fdd2e4e53b14cfd538f
Mohandinkar <mohandinkar75@gmail.com>
2025-01-16 18:44:59 +0530

    added

---
+++ LeetCode/LC-2425.cpp
@@ -0,0 +1,33 @@
+//2425 --> Bitwise XOR of All Pairings
+
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+    int n = nums1.size();
+    int m = nums2.size();
+
+    int ans = 0;
+
+    if(n%2 == 1 && m%2 == 1){
+        for(int i=0;i<n;i++){
+            ans = ans ^ nums1[i];
+        }
+         for(int i=0;i<m;i++){
+            ans = ans ^ nums2[i];
+        }
+
+    }
+    else if(n%2 == 1 && m%2 == 0){
+        for(int i = 0;i<m;i++){
+            ans = ans^nums2[i];
+        }
+    }
+    else if(n%2 == 0 && m%2 == 1){
+        for(int i = 0;i<n;i++){
+            ans = ans^nums1[i];
+        }
+    }
+    return ans;
+
+    }
+};
