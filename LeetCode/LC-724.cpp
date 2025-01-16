47fc36ee68eaebd037d56e79335c822408b6c01a
Mohandinkar <mohandinkar75@gmail.com>
2025-01-16 19:45:26 +0530

    added

---
+++ LeetCode/LC-724.cpp
@@ -0,0 +1,30 @@
+
+
+class Solution {
+public:
+    int prefixSum(vector<int>& nums){
+        vector<int>lsum(nums.size(),0);
+        vector<int>rsum(nums.size(),0);
+        
+        //calculate the left sum
+        for(int i=1;i<nums.size();i++){
+            lsum[i] = lsum[i-1] + nums[i-1];
+        }
+
+        //calculate the Right sum
+        for(int i=nums.size()-2;i>=0;i--){
+            rsum[i] = rsum[i+1] + nums[i+1];
+        }
+
+        //check
+        for(int i=0;i<nums.size();i++){
+            if(rsum[i] == lsum[i])
+            return i;
+
+        }
+        return -1;
+    }
+    int pivotIndex(vector<int>& nums) {
+       return prefixSum(nums); 
+    }
+};
