a73c0defdfaaed5268aef39fed01a374e9ac5773
Mohandinkar <mohandinkar75@gmail.com>
2025-01-18 17:14:35 +0530

    added

---
+++ LeetCode/LC-643.cpp
@@ -0,0 +1,25 @@
+//643 --> Maximum Average Subarray I
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int i=0, j = k-1;
+
+        int sum = 0;
+        for(int x=0;x<=j;x++){
+            sum+=nums[x];
+        }
+
+        int maxSum = sum;
+        j++;
+        while(j<nums.size()){
+            sum = sum - nums[i++];
+            sum = sum + nums[j++];
+
+            maxSum = max(maxSum, sum);
+        }
+        double avg = (double)maxSum/k;
+
+        return avg;
+    }
+};
