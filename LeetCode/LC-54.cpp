52e1a60c86e37b6002edad4512211992dd55dfcc
Mohandinkar <mohandinkar75@gmail.com>
2025-01-20 22:32:14 +0530

    added

---
+++ LeetCode/LC-54.cpp
@@ -0,0 +1,57 @@
+// 54 --> Spiral Matrix
+// Given an m x n matrix, return all elements of the matrix in spiral order.
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
+
+        int m = matrix.size(); //rowSize
+        int n = matrix[0].size(); //colSize
+        int total_elements = m * n;
+    
+        int startingRow = 0;
+        int endingCol = n-1;
+        int endingRow = m-1;
+        int startingCol = 0;
+
+        int count = 0;
+
+        while(count < total_elements){
+        //print starting row
+        for(int i=startingCol;i<=endingCol && count<total_elements;i++){
+            ans.push_back(matrix[startingRow][i]);
+            count++;
+        }
+
+        startingRow++;
+
+        //print end col
+        for(int i=startingRow;i<=endingRow && count<total_elements;i++){
+            ans.push_back(matrix[i][endingCol]);
+            count++;
+        }
+
+        endingCol--;
+
+        //print end row
+        for(int i=endingCol;i>=startingCol && count<total_elements;i--){
+            ans.push_back(matrix[endingRow][i]);
+            count++;
+        }
+
+        endingRow--;
+
+        //print start col
+        for(int i=endingRow;i>=startingRow && count<total_elements;i--){
+            ans.push_back(matrix[i][startingCol]);
+            count++;
+        }
+        
+        startingCol++;
+        }
+        return ans;
+   
+    }
+
+};
