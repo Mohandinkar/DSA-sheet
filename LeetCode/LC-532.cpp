d26de14a22728480ea576f68d40a0f5bd5049a1b
Mohandinkar <mohandinkar75@gmail.com>
2025-01-21 22:45:33 +0530

    added

--- LeetCode/LC-532.cpp
+++ LeetCode/LC-532.cpp
@@ -31,3 +31,41 @@ public:
       return ans.size();
     }
 };
+
+//--------OR--------
+//2nd method --> By using the Binary Search
+
+class Solution {
+public:
+    int binary_search(vector<int>&nums, int start , int x){
+        int end = nums.size()-1;
+        
+        while(start<=end){
+            int mid = (start+end)/2;
+
+            if(nums[mid] == x){
+                return mid;
+            }
+            else if(x > nums[mid]){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+
+    int findPairs(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+
+        set<pair<int,int>>ans;
+        for(int i=0;i<nums.size();i++){
+            if(binary_search(nums, i+1, nums[i]+k) != -1){
+                ans.insert({nums[i], nums[i]+k});
+            }
+        }
+        return ans.size();
+    }
+};
+
