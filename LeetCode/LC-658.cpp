045a1ee8188115357d1ab2352a4bf67f6d842295
Mohandinkar <mohandinkar75@gmail.com>
2025-01-25 19:43:50 +0530

    add

---
+++ LeetCode/LC-658.cpp
@@ -0,0 +1,28 @@
+// 658 --> Find K Closest Elements
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        // vector<int>ans;
+        int l=0, h=arr.size()-1;
+
+        while(h-l >= k){
+
+            if(x-arr[l] > arr[h]-x){
+                l++;
+            }
+            else{
+                h--;
+            }
+        }
+
+        // for(int i=l;i<=h;i++){
+        //     ans.push_back(arr[i]);
+        // }
+
+        return vector<int>(arr.begin()+l,arr.begin()+(h+1));
+    }
+};
+
+
+
