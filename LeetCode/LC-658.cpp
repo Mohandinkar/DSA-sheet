d6952b220ff64d05b2824f080e52aa078724f699
Mohandinkar <mohandinkar75@gmail.com>
2025-01-25 19:45:29 +0530

    add

--- LeetCode/LC-658.cpp
+++ LeetCode/LC-658.cpp
@@ -1,5 +1,6 @@
 // 658 --> Find K Closest Elements
 
+//1st method by using two pointers approach
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
@@ -24,5 +25,60 @@ public:
     }
 };
 
+
+//--------------OR---------------
+//2nd method by using the binary search method
+
+class Solution {
+public:
+
+    int lowerBound(vector<int>&arr, int x){
+        int st = 0, end = arr.size()-1;
+        int ans = end;
+
+        while(st <= end){
+            int mid = (st+end)/2;
+            if(arr[mid] >= x){
+                ans = mid;
+                end=mid-1;
+            }
+
+            else if(x > arr[mid]){
+                st = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+            
+        }
+        return ans;
+    }
+
+    vector<int>bs_method(vector<int>&arr,int k, int x){
+        int h = lowerBound(arr,x);
+        int l = h-1;
+
+        while(k--){
+            if(l<0){
+                h++;
+            }
+            else if(h >= arr.size()){
+                l--;
+            }
+            else if(x-arr[l] > arr[h]-x){
+                h++;
+            }
+            else{
+                l--;
+            }
+        }
+
+        return vector<int>(arr.begin()+(l+1), arr.begin()+h);
+    }
+
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+       return bs_method(arr,k, x);
+}
+};
 
 
