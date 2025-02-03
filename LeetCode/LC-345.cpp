94043b24cfcaa2743b6aaa6092a8f1a27ad60f86
Mohandinkar <mohandinkar75@gmail.com>
2025-02-03 22:20:41 +0530

    added

---
+++ LeetCode/LC-345.cpp
@@ -0,0 +1,33 @@
+
+//345 --> Reverse Vowels of a String
+
+class Solution {
+public:
+
+    bool isVowel(char ch){
+        ch = tolower(ch);
+        return ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u';
+    }
+    string reverseVowels(string s) {
+        
+        int l = 0;
+        int h = s.size() - 1;
+
+        while(l<=h){
+
+            if(isVowel(s[l]) && isVowel(s[h])){
+                swap(s[l], s[h]);
+                l++;
+                h--;
+            }
+            else if(!isVowel(s[l])){
+                l++;
+            }
+            else{
+                h--;
+            }
+        }
+
+        return s;
+    }
+};
