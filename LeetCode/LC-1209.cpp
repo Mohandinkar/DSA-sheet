f717413e46081ef9232cb6261a29120491875c4a
Mohandinkar <mohandinkar75@gmail.com>
2025-03-31 00:35:01 +0530

    added

--- LeetCode/LC-1209.cpp
+++ LeetCode/LC-1209.cpp
@@ -34,3 +34,34 @@ class Solution {
             return ans;
         }
     };
+
+// ----------------------OR--------------------
+//Optimized solution
+//Two Pointer apporached TC => O(n)
+
+class Solution {
+    public:
+       //two pointer 
+        string removeDuplicates(string s, int k) {
+          int i=0, j=0;
+          vector<int>count(s.size());
+    
+          while(j < s.size()){
+    
+            s[i] = s[j];
+    
+            count[i] = 1;
+    
+            if(i>0 && s[i] == s[i-1])
+                count[i] += count[i-1];
+            
+            if(count[i] == k)
+                i = i - k;
+    
+            i++, j++;
+          }
+    
+          return s.substr(0, i);
+    
+        }
+    };
