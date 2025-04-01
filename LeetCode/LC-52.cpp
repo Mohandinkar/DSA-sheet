949afe04fafd393eeae2a0360ffd4ce12917935e
Mohandinkar <mohandinkar75@gmail.com>
2025-04-01 20:16:41 +0530

    added

---
+++ LeetCode/LC-52.cpp
@@ -0,0 +1,74 @@
+//52 --> N-Queens II
+//count the number of to arrange the Queen so no two Queens attack each other
+
+class Solution {
+    public:
+    
+        unordered_map<int, bool>rowCheck;
+        unordered_map<int, bool>lowerDiagonalCheck;
+        unordered_map<int, bool>upperDiagonalCheck;
+    
+        bool isSafe(int row, int col, vector<vector<char>>&board){
+    
+            //check for row
+            if(rowCheck[row] == true){
+                return false;
+            }
+    
+            //for lowerDigonal
+            if(lowerDiagonalCheck[row+col] == true){
+                return false;
+            } 
+    
+            //for upperDiagonal
+            if(upperDiagonalCheck[row-col] == true){
+                return false;
+            }
+    
+            //else
+            return true;
+        }
+    
+        void solve(int n, vector<vector<char>>&board, vector<vector<string>>&ans, int col, int&count){
+            //base case
+            if(col >= n){
+                count++;
+                return;
+            }
+    
+            //solve 1 case 
+            for(int row=0;row<n;row++){
+    
+                if(isSafe(row,col,board)){
+                    board[row][col] = 'Q';
+                    rowCheck[row] = true;
+                    upperDiagonalCheck[row-col] = true;
+                    lowerDiagonalCheck[row+col] = true;
+    
+                    //recursion
+                    solve(n, board,ans, col+1, count);
+    
+                    //backtracking
+                    board[row][col] = '.';
+                    rowCheck[row] = false;
+                    upperDiagonalCheck[row-col]= false;
+                    lowerDiagonalCheck[row+col] = false;
+    
+                }
+            }
+        }
+    
+    
+        int totalNQueens(int n) {
+            vector<vector<string>>ans;
+    
+            vector<vector<char>>board(n, vector<char>(n,'.'));
+    
+            int col = 0;
+            int count = 0;
+            solve(n, board, ans, col,count);
+    
+            return count;
+    
+        }
+    };
