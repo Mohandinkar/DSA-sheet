f6e33b984bd0f5d6971d82c51ed13a0a127e238c
Mohandinkar <mohandinkar75@gmail.com>
2025-04-01 20:21:05 +0530

    Added

--- LeetCode/LC-52.cpp
+++ LeetCode/LC-52.cpp
@@ -1,5 +1,9 @@
 //52 --> N-Queens II
 //count the number of to arrange the Queen so no two Queens attack each other
+//The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that 
+//no two queens attack each other.
+
+Given an integer n, return the number of distinct solutions to the n-queens puzzle
 
 class Solution {
     public:
