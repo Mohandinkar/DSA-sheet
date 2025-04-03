2cb7651b5c422b997325c75ff6a3ccf5fc7557ed
Mohandinkar <mohandinkar75@gmail.com>
2025-04-03 22:56:49 +0530

    added

--- LeetCode/LC-912.cpp
+++ LeetCode/LC-912.cpp
@@ -40,3 +40,47 @@ class Solution {
             return nums;
         }
     };
+
+//-----------------------OR--------------------
+//Using Gap method for merge sort in Placed merge sort
+//Space complexity is O(1) in this case
+
+class Solution {
+    public:
+    
+        void inPlaceMerge(vector<int>&nums,  int start, int mid, int end){
+            int total_len = end - start + 1;
+            int gap = (total_len/2) + (total_len%2); //ceil => upper value 
+            while(gap > 0){
+                int i=start, j =start+gap;
+    
+                while(j<=end){
+                    if(nums[i] > nums[j]){
+                        swap(nums[i], nums[j]);
+                    }
+                    ++i, ++j;
+                }
+    
+                gap = gap<=1 ? 0 : (gap/2) + (gap%2);
+            }
+    
+    
+           
+        }
+        void mergeSort(vector<int>&nums, int start, int end){
+            //base case
+            if(start >= end) return ;
+    
+            int mid = (start+end)/2;
+            mergeSort(nums, start, mid);
+            mergeSort(nums, mid+1, end);
+    
+            inPlaceMerge(nums, start, mid, end);
+        }
+        vector<int> sortArray(vector<int>& nums) {
+           
+    
+            mergeSort(nums, 0, nums.size()-1);
+            return nums;
+        }
+    };
