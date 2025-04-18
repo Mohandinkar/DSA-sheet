39884016d9ece06ab8ea15f2c0c8f7a0cb3f16f9
Mohandinkar <mohandinkar75@gmail.com>
2025-04-18 12:03:55 +0530

    added

--- LeetCode/LC-141.cpp
+++ LeetCode/LC-141.cpp
@@ -29,3 +29,25 @@ class Solution {
              return false;
         }
     };
+
+//-----------------OR----------------
+class Solution {
+    public:
+        bool hasCycle(ListNode *head) {
+           ListNode* slow = head;
+           ListNode* fast = head;
+    
+           while(fast != NULL){
+    
+                fast = fast->next;
+                if(fast != NULL){
+                    fast = fast->next;
+                    slow = slow->next;
+                    
+                    if(fast == slow) return true;
+                }
+           }
+    
+           return false;
+        }
+    };
