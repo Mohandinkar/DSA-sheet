284398e5dd935ed2239e6909b875e36b6909a39c
Mohandinkar <mohandinkar75@gmail.com>
2025-04-29 19:56:28 +0530

    added

--- LeetCode/LC-2.cpp
+++ LeetCode/LC-2.cpp
@@ -42,3 +42,35 @@ class Solution {
            return iterative(l1, l2);
         }
     };
+
+//---------OR----------
+//BY USING RECURSION
+
+class Solution {
+    public:
+        ListNode* recursive(ListNode* l1, ListNode* l2, int carry = 0){
+    
+            //base case
+            if(!l1 && !l2 && !carry) return 0;
+    
+            //solve case1
+            int a = l1 ? l1->val : 0;
+            int b = l2 ? l2->val : 0;
+            int sum = a+b+carry;
+    
+            int digit = sum%10;
+            carry = sum/10;
+    
+            ListNode* ans = new ListNode(digit);
+            //recursive call
+            ans->next = recursive(l1 ? l1->next :l1, l2 ? l2->next : l2, carry);
+    
+            return ans;
+    
+           
+        }
+    
+        ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+           return recursive(l1, l2);
+        }
+    };
