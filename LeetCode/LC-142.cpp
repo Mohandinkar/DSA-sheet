faa08809a6c63187fbfbc3ef1a93fc3f844045ad
Mohandinkar <mohandinkar75@gmail.com>
2025-04-30 20:26:25 +0530

    added

--- LeetCode/LC-142.cpp
+++ LeetCode/LC-142.cpp
@@ -27,3 +27,42 @@ class Solution {
             return NULL;
         }
     };
+
+//--------------OR-----------------
+//By using the Slow and fast pointer
+
+class Solution {
+    public:
+        ListNode* hasCycle(ListNode*&head){
+            ListNode* fast = head;
+            ListNode* slow = head;
+    
+            while(fast != NULL){
+                fast = fast->next;
+    
+                if(fast != NULL){
+                    fast = fast->next;
+                    slow = slow->next;
+    
+                    if(slow == fast)
+                    return fast;
+                }
+            }
+            return nullptr;
+        }
+    
+        ListNode *detectCycle(ListNode *head) {
+            ListNode* fast = hasCycle(head);
+            ListNode* slow = head;
+    
+            if(!fast) return nullptr;
+    
+            //if cycle exist then move slow & fast both by one step to get start point of the cycle
+            while(slow != fast){
+                slow = slow->next;
+                fast = fast->next;
+           }
+    
+           return slow;
+        }
+    };
