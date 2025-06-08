99377b335c24a4744a04ab82c29ecbd8c836a88b
Mohandinkar <mohandinkar75@gmail.com>
2025-06-08 21:45:18 +0530

    added

---
+++ LeetCode/Lc-20.cpp
@@ -0,0 +1,38 @@
+//20. Valid Parentheses
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+
+        for(int i=0;i<s.length();i++){
+
+            char ch = s[i];
+
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }
+
+            else{
+                if(st.empty()) return false;
+
+                if((ch == ')' && st.top() == '(') || (ch == ']' && st.top() == '[') || (ch == '}' && st.top() == '{')){
+
+                    st.pop();
+
+                }
+                else{
+
+                    return false;
+                }
+            }
+        }
+
+        if(st.size()){
+            return false;
+        }else{
+            return true;
+        }
+    }
+};
