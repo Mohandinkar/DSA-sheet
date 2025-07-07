d82bd2f2f82020cee078c76cdb80b6224f201f9f
Mohandinkar <mohandinkar75@gmail.com>
2025-07-07 20:15:13 +0530

    LC-134

--- LeetCode/LC-134.cpp
+++ LeetCode/LC-134.cpp
@@ -26,3 +26,34 @@ public:
         }
     }
 };
+
+//-------------------OR-----------------
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int balance = 0;
+        int deficit = 0;
+        int start = 0;
+
+        for(int i=0;i<gas.size();i++){
+
+            if(balance + gas[i] < cost[i]){
+                //deficit
+                deficit += abs(balance + gas[i] - cost[i]);
+
+                start = i+1;
+                balance = 0;
+            }
+            else{
+                balance += gas[i] - cost[i];
+            }
+        }
+
+        if(balance - deficit >= 0){
+            return start;
+        }
+        else
+            return -1;
+    }
+};
