c9ec7779f49123cd771c9bb2dca8f909d131c643
Mohandinkar <mohandinkar75@gmail.com>
2025-07-17 20:03:59 +0530

    LC-121

--- LeetCode/LC-121.cpp
+++ LeetCode/LC-121.cpp
@@ -22,3 +22,37 @@ public:
         return maxProfit;
     }
 };
+
+//-------------OR--------------
+//using RE but increases the Space Complexity O(n)
+
+class Solution {
+public:
+
+    void solve(vector<int>&prices, int i, int&minPrice, int&maxProfit){
+        //base case
+        if(i == prices.size())
+            return;
+            
+        //solve one case
+        if(prices[i] < minPrice) 
+            minPrice = prices[i];
+        
+        int currProfit = prices[i] - minPrice;
+
+        if(currProfit > maxProfit)
+            maxProfit = currProfit;
+
+        //RE
+        solve(prices, i+1, minPrice, maxProfit);  
+    }
+
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxProfit = INT_MIN;
+
+        solve(prices, 0, minPrice, maxProfit);
+
+        return maxProfit;
+    }
+};
