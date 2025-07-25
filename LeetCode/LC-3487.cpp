fd25aff985aaf37feda428801ff536054400f9a4
Mohandinkar <mohandinkar75@gmail.com>
2025-07-25 23:02:31 +0530

    LC-3487

---
+++ LeetCode/LC-3487.cpp
@@ -0,0 +1,29 @@
+//3487 --> Maximum Unique Subarray Sum After Deletion
+
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        unordered_map<int, int>mp;
+
+        int maxNeg = INT_MIN;
+        int sum = 0;
+
+        for(auto num : nums){
+
+            if(num <= 0){
+                maxNeg = max(maxNeg, num);
+            }
+            //check if num already present or not
+            else if(mp.find(num) == mp.end()){
+                sum += num;
+                mp[num] = num;
+            }
+        }
+
+        return sum ? sum : maxNeg;
+    }
+};
+
+//TC: O(n)
+//SC: O(n)
+
