500731588b87457e9ac34134caa582c91bb619e7
Mohandinkar <mohandinkar75@gmail.com>
2025-07-25 23:02:49 +0530

    LC-3487

--- LeetCode/LC-3487.cpp
+++ LeetCode/LC-3487.cpp
@@ -27,3 +27,28 @@ public:
 //TC: O(n)
 //SC: O(n)
 
+//-------OR-----------
+//using set
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        unordered_set<int>set;
+
+        int maxNeg = INT_MIN;
+        int sum = 0;
+
+        for(auto num : nums){
+
+            if(num <= 0){
+                maxNeg = max(maxNeg, num);
+            }
+            //check if num already present or not
+            else if(!set.count(num)){
+                sum += num;
+                set.insert(num);
+            }
+        }
+
+        return sum ? sum : maxNeg;
+    }
+};
