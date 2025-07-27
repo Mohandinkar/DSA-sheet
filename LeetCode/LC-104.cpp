38b129bda76cd7e9f215145bf025721dbcd5e6fc
Mohandinkar <mohandinkar75@gmail.com>
2025-07-28 00:37:47 +0530

    LC-104

---
+++ LeetCode/LC-104.cpp
@@ -0,0 +1,27 @@
+//104 --> Maximum Depth of Binary Tree
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        
+        int leftSubTreeHeight = 1 + maxDepth(root->left);
+        int rightSubTreeHeight = 1 + maxDepth(root->right);
+        int maxHeight = max(leftSubTreeHeight, rightSubTreeHeight);
+
+        return maxHeight;
+        
+    }
+};
