00f1131fe421c168dc48ec9c389995abd8531c14
Mohandinkar <mohandinkar75@gmail.com>
2025-09-10 00:16:18 +0530

    BST kth last element

--- LeetCode/LC-230.cpp
+++ LeetCode/LC-230.cpp
@@ -32,3 +32,43 @@ public:
         return inorder[k-1];
     }
 };
+
+//----------------OR-----------------
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void solve(TreeNode*root, int &k, TreeNode*&ans){
+        if(!root){
+            return;
+        }
+
+        //L-N-R Inoder
+        solve(root->left, k, ans);
+
+        k--;
+        if(!k) {     
+            ans = root;
+            return;
+        }
+        
+        solve(root->right, k, ans);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        TreeNode* ans = NULL;
+        solve(root, k, ans);
+
+        return ans->val;
+    }
+};
