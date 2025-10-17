a7571ae75a0915fe9bdee87610a7a5a2f653d0cd
Mohandinkar <mohandinkar75@gmail.com>
2025-10-17 10:42:26 +0530

    Wildcard Matching(Tabulation)

--- LeetCode/DP/LC-44.cpp
+++ LeetCode/DP/LC-44.cpp
@@ -45,8 +45,48 @@ public:
         return dp[i][j];
     }
 
+        //using Bottom-Up approach(iterative)
+    bool solveUsingTab(string s, string p, int m, int n){
+
+        vector<vector<int>>dp(m+1, vector<int>(n+1, 0));
+
+        //base case analysis
+        dp[m][n] = 1;
+
+        for(int col=0;col<n;col++){
+            bool flag = true;
+             for(int k=col;k<p.length();k++){
+                if(p[k] != '*'){
+                    flag = false;
+                    break;
+                }
+            }
+            dp[m][col] = flag;
+        }
+
+        for(int i=m-1;i>=0;i--){
+            for(int j=n;j>=0;j--){
+                bool ans;
+                if(p[j] == '?' || s[i] == p[j]){
+                    ans = dp[i+1][j+1];
+                }
+                else if(p[j] == '*'){
+
+                    ans = dp[i+1][j] || dp[i][j+1];
+                }
+                else{
+                    ans = false;
+                }
+                dp[i][j] = ans;
+            }
+        }
+
+        return dp[0][0];
+    }
+
     bool isMatch(string s, string p) {
-        
+        int m = s.length();
+        int n = p.length();
         vector<vector<int>>dp(s.length()+1, vector<int>(p.length()+1, -1));
 
         return solveMemo(s, p, 0, 0, dp);
